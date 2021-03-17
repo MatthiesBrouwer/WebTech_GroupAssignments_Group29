13947b636e81453514f657734b62cf0795e09106
Matthies <ties1000@live.nl>
2021-03-17 12:43:32 +0100

    adding comments

--- Assignment_2/JS/header-bar-functionalities.js
+++ Assignment_2/JS/header-bar-functionalities.js
@@ -1,9 +1,9 @@
 
-
-function handleDropdownMenu(dropdownElementId, dropdownElement){
+// Function to handle dropdown interactivity
+function handleDropdownMenu(dropdownElementId, dropdownElement) {
     var dropdownElement = document.getElementById(dropdownElementId);
     console.log(dropdownElement);
-    if(dropdownElement.classList.contains(dropdownElementId + "--dropdown-active")) {
+    if (dropdownElement.classList.contains(dropdownElementId + "--dropdown-active")) {
         dropdownElement.classList.remove(dropdownElementId + "--dropdown-active")
         dropdownElement.classList.add(dropdownElementId + "--dropdown-inactive")
     }
@@ -20,68 +20,63 @@ function fillModificationMenuSemanticPicker() {
 
     var semanticElements = ["BODY", "HEADER", "ASIDE", "FOOTER", "ARTICLE", "SECTION"];
 
+    /* Check for each element if it exists on the page and index them accordingly */
     for (let i = 0; i < semanticElements.length; i++) {
         semanticList = document.getElementsByTagName(semanticElements[i])
-        for (let j = 0; j < semanticList.length; j++){
+        for (let j = 0; j < semanticList.length; j++) {
             var newOption = document.createElement("option");
             newOption.value = semanticElements[i] + "-" + j;
+            newOption.text = semanticElements[i].toLocaleLowerCase() + (semanticList.length > 1 ? "-" + j : "" );
+            semanticPicker.add(newOption);
 
-            if (semanticList.length > 1){
-                if (j == 0){
+            /* Index elements in an optgroup if more than 1 exists on the page. Otherwise simply index them */
+            if (semanticList.length > 1) {
+                if (j == 0) {
                     var newOptgroup = document.createElement("OPTGROUP");
                     newOptgroup.label = semanticElements[i].toLocaleLowerCase();
                 }
-                newOption.text = semanticElements[i].toLocaleLowerCase() + "-" + j;
-                semanticPicker.add(newOption);
-                newOptgroup.appendChild(newOption);   
-            }
-            else{
-                newOption.text = semanticElements[i].toLocaleLowerCase();
-                semanticPicker.add(newOption);
+                newOptgroup.appendChild(newOption);
             }
         }
-        if (semanticList.length > 1){
-            semanticPicker.insertBefore(newOptgroup, semanticPicker.options[semanticPicker.length - semanticList.length -i]);
+        if (semanticList.length > 1) {
+            /* Index the opgroup of the current semantic element just before the index of the first element of the current semantic */
+            semanticPicker.insertBefore(newOptgroup, semanticPicker.options[semanticPicker.length - semanticList.length - i]);
         }
     }
 
     console.log(semanticPicker);
 }
 
-
+// Function to process any style changes by the user
 function processStyleModifications(event) {
     event.preventDefault();
 
-    /* get the picked semantic element */
+    /* get the picked semantic element name and the actual element.
+        The semantic name containes a tag name and it's quantity index, divided by a "-" */
     var pickedSemantic = document.getElementById("page-editor__semantic-selector").value.split("-");
-
-    /* Get the selected style changes from the user */
-    var newBackgroundColor = document.forms["page-editor"]["page-editor__background-color-selector"].value;
-    var newFontSize = document.forms["page-editor"]["page-editor__font-size-selector"].value;
-
-
     var pickedSemanticElement = document.getElementsByTagName(pickedSemantic[0])[pickedSemantic[1]];
 
-    console.log("setting font size for " + pickedSemantic[0] + " to " + newFontSize + "px");
+    /* Get the selected style changes from the user and set the new styles*/
+    var newBackgroundColor = document.forms["page-editor"]["page-editor__background-color-selector"].value;
+    var newFontSize = document.forms["page-editor"]["page-editor__font-size-selector"].value;
     pickedSemanticElement.style.fontSize = newFontSize + "px";
-
-    console.log("setting background color for " + pickedSemantic[0] + "from " + pickedSemanticElement.style.backgroundColor + "to " + newBackgroundColor);
     pickedSemanticElement.style.backgroundColor = newBackgroundColor;
 
 }
 
 
-function registerEvents(){
+// Function to register all events
+function registerEvents() {
 
     window.addEventListener('load', fillModificationMenuSemanticPicker, false);
 
-    var modificationMenu = document.forms["page-editor"].addEventListener('submit', processStyleModifications, false);
+    document.forms["page-editor"].addEventListener('submit', processStyleModifications, false);
 
-    var stylePickerDropdownButton =  document.getElementById("page-editor__dropdown-button");
-    stylePickerDropdownButton.addEventListener("click", function(){ handleDropdownMenu("page-editor__menu");}, true);
-    
+    var stylePickerDropdownButton = document.getElementById("page-editor__dropdown-button");
+    stylePickerDropdownButton.addEventListener("click", function () { handleDropdownMenu("page-editor__menu"); }, true);
+
     var menubarDropdownButton = document.getElementById("header-bar__dropdown-menu-button");
-    menubarDropdownButton.addEventListener("click", function(){ handleDropdownMenu("header-bar__menu");}, true);
+    menubarDropdownButton.addEventListener("click", function () { handleDropdownMenu("header-bar__menu"); }, true);
 
 }
 
