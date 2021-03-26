9aad94c7d7072e6609c12694b2112591a2d64001
Matthies <ties1000@live.nl>
2021-03-26 15:57:48 +0100

    created initial server setup

---
+++ Assignment_3/myapp/myapp/app.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const path = require("path");
+var logger = require('morgan');
+
+const app = express();
+
+var staticPath = path.join(__dirname, "static");
+var publicPath = path.join(__dirname, 'public');
+app.use(express.static(staticPath));
+app.use(express.static(publicPath));
+
+app.use(logger('dev'));
+
+console.log("LOADED");
+
+module.exports = app;
+
+/*const apiRouter = require("./routes/api_router");
+
+app.use("/api", apiRouter);*/
+
+
+/*var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;*/
